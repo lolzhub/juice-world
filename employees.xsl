d6e5d128190ba6da5125d05492ea8818492b82b0
anon <125595392+lolzhub@users.noreply.github.com>
2023-05-21 22:53:45 +0530

    final commit

---
+++ employees.xsl
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+
+  <xsl:template match="/">
+    <html>
+      <head>
+        <title>Employees</title>
+        <style>
+          table {
+            border-collapse: collapse;
+            width: 100%;
+          }
+          th, td {
+            text-align: left;
+            padding: 8px;
+            border-bottom: 1px solid #ddd;
+          }
+          th {
+            background-color: #4CAF50;
+            color: white;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>Employees</h1>
+        <table>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Salary</th>
+          </tr>
+          <xsl:for-each select="employees/employee">
+            <tr>
+              <td><xsl:value-of select="id"/></td>
+              <td><xsl:value-of select="name"/></td>
+              <td><xsl:value-of select="salary"/></td>
+            </tr>
+          </xsl:for-each>
+        </table>
+      </body>
+    </html>
+  </xsl:template>
+
+</xsl:stylesheet>
--- form-handler.php
+++ form-handler.php
@@ -1,27 +1,45 @@
-<?php
-$name = $_POST['name'];
-$visitor = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+<!DOCTYPE html>
+<html lang="en">
 
-$email_from = 'pwnxd@pm.me';
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-$email_subject = 'New submission form';
-
-$email_body = "User name: $name.\n".
-                "User email: $visitor.\n".
-                "User subject: $subject.\n".
-                "User message: $message.\n";
-
-$to = 'pwnxd@pm.me';
-
-$headers = "From: $email_from \r\n";
-
-$headers .="Reply to: $visitor \r\n";
-
-mail($to, $email_subject, $email_body, $headers);
-
-header("Location: contact.html")
+<body>
+    <?php
 
 
-?>
+    $con = mysqli_connect("localhost", "root", "", "juiceworld");
+    if (!$con) {
+    ?>
+        <script type="text/javascript">
+            alert("Smtn went wrong...");
+        </script>
+
+    <?php
+        include("index.html");
+    }
+    $name = $_REQUEST['name'];
+    $visitor = $_REQUEST['email'];
+    $subject = $_REQUEST['subject'];
+    $message = $_REQUEST['message'];
+
+    $count = mysqli_query($con, "insert into contactus values ('$name' , '$visitor' , '$subject' , '$message');");
+    if ($count >= 1) {
+    ?>
+        <script type="text/javascript">
+            alert("Message sent successfully");
+        </script>
+
+    <?php
+        include("index.html");
+    }
+
+
+    ?>
+</body>
+
+</html>
